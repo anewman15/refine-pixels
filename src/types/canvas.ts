b8b9a3eb823e4e4795d4a4b8f0c9f4500637444c
Abdullah Numan <anewman15@hotmail.com>
2022-12-01 17:28:14 +0600

    Add types

---
+++ src/types/canvas.ts
@@ -0,0 +1,8 @@
+export type Canvas = {
+  id: string;
+  name?: string;
+  width: number;
+  height: number;
+  created_at: string;
+  password?: string;
+};
---
+++ src/types/pixel.ts
@@ -0,0 +1,9 @@
+export type Pixel = {
+  id: string;
+  canvas_id: string;
+  x: number;
+  y: number;
+  color: string;
+  created_at: string;
+  name?: string;
+};
