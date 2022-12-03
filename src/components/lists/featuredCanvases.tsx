273716b2ad899ab3eeb44a570d633735ecb398b7
Abdullah Numan <anewman15@hotmail.com>
2022-12-04 00:00:05 +0600

    Refactor FeaturedCanvases with CanvasTile

--- src/components/lists/featuredCanvases.tsx
+++ src/components/lists/featuredCanvases.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import { useList, useNavigation } from '@pankod/refine-core';
-import { Button, Col, Empty, Row, Typography } from '@pankod/refine-antd';
+import { useList } from '@pankod/refine-core';
+import { Empty, Row } from '@pankod/refine-antd';
 import { Canvas } from 'types/canvas';
-import { CanvasItem } from '..';
-
-const { Title } = Typography;
+import CanvasTile from 'components/canvas/tile';
 
 const FeaturedCanvases = () => {
   const { data } = useList<Canvas>({
@@ -27,55 +25,27 @@ const FeaturedCanvases = () => {
     },
   });
 
-  const { show } = useNavigation();
 
   return (
     <>
-      <Title level={2}>Featured</Title>
-      {
-        data?.data ? (
-          <Row
-            gutter={[16, 16]}
-            align="middle"
-            style={{
-              display: "grid",
-              gridTemplateColumns: "repeat(auto-fill, minmax(248px, 1fr))",
-              alignItems: "center",
-              justifyItems: "center",
-            }}
-          >
-            {data?.data?.map((canvas) => (
-              <Col
-                key={canvas.id}
-                style={{
-                  height: "100%",
-                  width: "100%",
-                  maxWidth: "248px",
-                }}
-              >
-                <Button
-                  onClick={() => {
-                    show("canvases", canvas.id);
-                  }}
-                  style={{
-                    height: "100%",
-                    maxHeight: "unset",
-                    paddingTop: "15px",
-                    display: "flex",
-                    alignItems: "start",
-                  }}
-                >
-                  <CanvasItem
-                    canvas={canvas}
-                    scale={20 / canvas.width}
-                    active={false}
-                  />
-                </Button>
-              </Col>
-            ))}
-          </Row>
-        ) : <Empty />
-      }
+      {data?.data ? (
+        <Row
+          gutter={[16, 16]}
+          align="middle"
+          style={{
+            display: "grid",
+            gridTemplateColumns: "repeat(4, minmax(248px, 1fr))",
+            alignItems: "start",
+            justifyItems: "center",
+          }}
+        >
+          {data?.data?.map((canvas) => (
+            <CanvasTile canvas={canvas} />
+          ))}
+        </Row>
+      ) : (
+        <Empty />
+      )}
     </>
   );
 };
