95486412d333969ee6c03ef3b1cc765bf051a210
Abdullah Numan <anewman15@hotmail.com>
2022-12-03 10:28:55 +0600

    Add banner to CanvasList

--- src/pages/canvases/list.tsx
+++ src/pages/canvases/list.tsx
@@ -1,11 +1,11 @@
 import {
   Button,
   Col,
-  CreateButton,
   Row,
   Typography,
 } from "@pankod/refine-antd";
 import { useList, useNavigation, useResource } from "@pankod/refine-core";
+import SponsorsBanner from "components/banners/sponsors";
 import { CanvasItem } from "components/canvas";
 import { Canvas } from "types/canvas";
 
@@ -23,58 +23,83 @@ export const CanvasList = () => {
           order: "desc",
         },
       ],
+      filters: [
+        {
+          field: "is_featured",
+          operator: "eq",
+          value: true,
+        },
+      ],
     },
   });
 
   const { show } = useNavigation();
 
   return (
-    <div className="page-shadow">
-      <Row style={{ marginBottom: "24px" }}>
-        <Col flex="1">
-          <Typography.Title level={3}>{label ?? name}</Typography.Title>
-        </Col>
-      </Row>
+    <Row
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+      }}
+    >
       <Row
-        gutter={[16, 16]}
-        align="middle"
+        className="page-shadow"
         style={{
-          display: "grid",
-          gridTemplateColumns: "repeat(auto-fill, minmax(248px, 1fr))",
-          alignItems: "center",
-          justifyItems: "center",
+          display: "flex",
+          flexDirection: "column",
+          width: "100%",
         }}
       >
-        {data?.data?.map((canvas) => (
-          <Col
-            key={canvas.id}
-            style={{
-              height: "100%",
-              width: "100%",
-              maxWidth: "248px",
-            }}
-          >
-            <Button
-              onClick={() => {
-                show("canvases", canvas.id);
-              }}
+        <Row style={{ marginBottom: "24px" }}>
+          <Col flex="1">
+            <Typography.Title level={3}>{label ?? name}</Typography.Title>
+          </Col>
+        </Row>
+        <Row
+          gutter={[16, 16]}
+          align="middle"
+          style={{
+            display: "grid",
+            gridTemplateColumns: "repeat(auto-fill, minmax(248px, 1fr))",
+            alignItems: "center",
+            justifyItems: "center",
+          }}
+        >
+          {data?.data?.map((canvas) => (
+            <Col
+              key={canvas.id}
               style={{
                 height: "100%",
-                maxHeight: "unset",
-                paddingTop: "15px",
-                display: "flex",
-                alignItems: "start",
+                width: "100%",
+                maxWidth: "248px",
               }}
             >
-              <CanvasItem
-                canvas={canvas}
-                scale={20 / canvas.width}
-                active={false}
-              />
-            </Button>
-          </Col>
-        ))}
+              <Button
+                onClick={() => {
+                  show("canvases", canvas.id);
+                }}
+                style={{
+                  height: "100%",
+                  maxHeight: "unset",
+                  paddingTop: "15px",
+                  display: "flex",
+                  alignItems: "start",
+                }}
+              >
+                <CanvasItem
+                  canvas={canvas}
+                  scale={20 / canvas.width}
+                  active={false}
+                />
+              </Button>
+            </Col>
+          ))}
+        </Row>
       </Row>
-    </div>
+      <SponsorsBanner />
+    </Row>
   );
 };
