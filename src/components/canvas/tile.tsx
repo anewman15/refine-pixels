aef4970cffeb27e909ba06b067d77b925a91bb21
Abdullah Numan <anewman15@hotmail.com>
2022-12-06 09:47:50 +0600

    Add Contributors to CanvasTile

--- src/components/canvas/tile.tsx
+++ src/components/canvas/tile.tsx
@@ -1,8 +1,10 @@
-import { Button, Col } from '@pankod/refine-antd';
+import { Button, Col, Space } from '@pankod/refine-antd';
 import { useNavigation } from '@pankod/refine-core';
+import Contributors from 'components/avatar/contributors';
 import React from 'react'
 import { Canvas } from 'types/canvas';
 import { CanvasItem } from '.';
+import DisplayCanvas from './display';
 
 type CanvasTileProps = {
   canvas: Canvas;
@@ -12,29 +14,41 @@ const CanvasTile: React.FC<CanvasTileProps> = ({ canvas }) => {
   const { show } = useNavigation();
 
   return (
-    <Col
-      key={canvas.id}
-      style={{
-        height: "auto",
-        width: "100%",
-        maxWidth: "248px",
-      }}
-    >
-      <Button
-        onClick={() => {
-          show("canvases", canvas.id);
-        }}
-        style={{
-          height: "auto",
-          maxHeight: "auto",
-          paddingTop: "15px",
-          display: "flex",
-          alignItems: "stretch",
-        }}
-      >
-        <CanvasItem canvas={canvas} scale={20 / canvas.width} active={false} />
-      </Button>
-    </Col>
+    <>
+      <DisplayCanvas canvas={canvas}>
+        {(pixels) => (
+          <Col
+            key={canvas.id}
+            style={{
+              height: "auto",
+              width: "100%",
+              maxWidth: "248px",
+            }}
+          >
+            <Button
+              onClick={() => {
+                show("canvases", canvas.id);
+              }}
+              style={{
+                height: "auto",
+                maxHeight: "auto",
+                paddingTop: "15px",
+                display: "flex",
+                alignItems: "stretch",
+              }}
+            >
+              <CanvasItem
+                canvas={canvas}
+                pixels={pixels}
+                scale={20 / canvas.width}
+                active={false}
+              />
+            </Button>
+            <Contributors pixels={pixels} />
+          </Col>
+        )}
+      </DisplayCanvas>
+    </>
   );
 };
 
